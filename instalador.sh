10948d2c66d11aea1116aa0a4f34d9af1809ce8b
carrillo2202 <131916828+carrillo2202@users.noreply.github.com>
2024-11-28 18:47:22 +0000

    Archivo instalador bash

---
+++ instalador.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Author: Brayan Carrillo <example@examploe.com>
+
+sudo apt update -y && sudo apt upgrade -y
+
+#wget  https://images.pexels.com/photos/1933239/pexels-photo-1933239.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
+
+sudo apt install default-jre
+
+sudo apt install default-jdk
+
+sudo apt install php8.3 -y
